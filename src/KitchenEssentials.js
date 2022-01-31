a182ce7397830b16e9ed2361e99724cf7290c2bf
OrkoHarsh <harsh_sharma69@outlook.com>
2022-01-31 19:14:35 +0530

    new commit

---
+++ src/KitchenEssentials.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import "./KitchenEssentials.css";
+
+function Checkout() {
+  return (
+  <div className='Dailly'>
+        <div className='dailly_ad'>
+          <img className='ad'
+           src='https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/JAN-ART-22/Bank/New/bank_stripe_elongated_pc_EN.jpg'
+           alt=''/>
+        </div>
+        < div className='gridcontainer'>
+            <div className='roww1'>
+                <img className='imgg1' 
+                
+                src='https://m.media-amazon.com/images/I/51RXzjrUmkL._AC_UL320_.jpg'
+                alt=''
+                />
+            <div className='inf'>
+                <div className='detailss'>
+                <h2>Pigeon Polypropylene Mini Handy and Compact Chopper with 3 Blades for Effortlessly Chopping Vegetables and Fruits</h2>
+                <div className='pricee'>
+                <ul>
+                <li className='kll'>M.R.P :₹549.00</li>
+                <li className='kl'>Deal Price:₹249.00</li>
+                <li className='kdisc'>You Save :₹300</li>
+                <li className='klll'>Inclusive of all taxes</li>
+                </ul></div>
+                <div className='buttonn'>
+                <button className='btnn'>ADD TO CART</button>
+                <br/></div>
+
+
+
+                <strong>About this item</strong>
+                <p>Large Handy Chopper is made from Polypropylene (PP) for long-lasting use
+Chopper comes with Sturdy 3-blade design made from Stainless Steel, As this chopper by Pigeon can be opened and detached easily, cleaning is an easy task using lukewarm water, mild detergent and a soft cloth
+The unique string function of chooper makes vegetables and fruits chopping easy</p>
+                    
+                </div>
+            </div>    
+            </div>   
+
+
+            <div className='roww2'>
+                <img className='imgg2'
+                src='https://m.media-amazon.com/images/I/51sF1z2PSMS._AC_UL320_.jpg' 
+                alt=''/>
+            <div className='inf'>
+                <div className='detailss'>
+                <h2>Aquaqlean Frosty Plastic Food Grade Fridge Square Water Bottle Set(6 Pieces, 1L,Assorted)</h2>
+                <div className='pricee'>
+                <ul>
+                <li className='kll'>M.R.P :₹1080.00</li>
+                <li className='kl'>Deal Price:₹680.00</li>
+                <li className='kdisc'>You Save :₹400</li>
+                <li className='klll'>Inclusive of all taxes</li>
+                </ul></div>
+                <div className='buttonn'>
+                <button className='btnn'>ADD TO CART</button>
+                <br/></div>
+
+
+
+                <strong>About this item</strong>
+                <p>Hygienic bottle: the bottle is made of 100% food grade polypropylene (pp) material from source which ensures a safer drinking than pet bottles
+Hygienic bottle: these bottles are sleek & compatible with refrigerator and major other bottle storage compartments in car too.the bottle not being curvy occupies less space and looks elegant in the fridge or on the table top.
+Leak proof: the bottle has a liquid tight cap with silicon protection that prevents leakage.</p>
+                    
+                </div>
+            </div>  
+
+
+            </div>
+
+
+            <div className='roww3'>
+                <img className='imgg3'
+                src='https://m.media-amazon.com/images/I/71BFmjNj6HL._AC_UL320_.jpg'
+                alt=''/>
+                           <div className='inf'>
+                <div className='detailss'>
+                <h2>Max Home Magic Silicone Cleaning Hand Gloves for Kitchen Dishwashing and Pet Grooming, Washing Dish, Car, Bathroom</h2>
+                <div className='pricee'>
+                <ul>
+                <li className='kll'>M.R.P :₹999.00</li>
+                <li className='kl'>Deal Price:₹2990.00</li>
+                <li className='kdisc'>You Save :₹700</li>
+                <li className='klll'>Inclusive of all taxes</li>
+                </ul></div>
+                <div className='buttonn'>
+                <button className='btnn'>ADD TO CART</button>
+                <br/></div>
+
+
+
+                <strong>About this item</strong>
+                <p>Multipurpose Hand Gloves for Kitchen Dishwashing etc
+Item Weight : 100 g
+Safe&Reusable</p>
+                    
+                </div>
+            </div>  
+             </div>
+            
+
+
+            <div className='roww4'>
+                <img className='imgg4'
+                src='  https://m.media-amazon.com/images/I/71R8GV2pxaL._AC_UL320_.jpg'
+                alt=''/>
+                           <div className='inf'>
+                <div className='detailss'>
+                <h2>Golwyn® Kitchen Accessories Items Dispenser Soap Pump Sponge Holder Plastic Liquid Soap Dispenser Kitchen Set</h2>
+                <div className='pricee'>
+                <ul>
+                <li className='kll'>M.R.P :₹380.00</li>
+                <li className='kl'>Deal Price:₹220.00</li>
+                <li className='kdisc'>You Save :₹160</li>
+                <li className='klll'>Inclusive of all taxes</li>
+                </ul></div>
+                <div className='buttonn'>
+                <button className='btnn'>ADD TO CART</button>
+                <br/></div>
+
+
+
+                <strong>About this item</strong>
+                <p>Multifunction Use Best for home, school, office, hospital, hotel, restaurant, factories, airport, gas station, and so on. Ideal for commercial and public Places.
+SPEND LESS TIME WASHING UP-We designed our soap dispenser to help reduce the time you spend at the kitchen sink. Effortless 1-hand dispensing operation allows you to wash dishes faster, whilst reducing the space taken up on your sink.</p>
+                    
+                </div>
+            </div>  
+                
+                
+                
+            </div> 
+        </div>
+
+      
+
+
+
+
+
+
+
+
+
+  </div>
+
+  );
+}
+
+export default Checkout;
